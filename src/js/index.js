3393760e297b9f4379ba60113dad9f5a1e568406
John Tasto <john@tasto.net>
2016-06-04 01:47:35 -0700

    Get parallax scrolling working

--- src/js/index.js
+++ src/js/index.js
@@ -4,22 +4,29 @@ import AnimationFrame from 'animation-frame'
 const animationFrame = new AnimationFrame()
 
 let windowHeight = window.innerHeight
-let lastScrollY = 0
+let windowMidY = windowHeight / 2
+let scrollY = 0
 let hasRequestedFrame = false
 const photos = document.querySelectorAll('.window-contents img')
-const speedDivider = 2
+const blurbs = document.querySelectorAll('.window-contents p')
 
 function updatePositions() {
-  //translateY(bgElm, lastScrollY / speedDivider)
+  let scrollCenterY = scrollY + windowHeight
   for (let photo of photos) {
-    // console.log(photo.getBoundingClientRect())
+    let bounds = photo.getBoundingClientRect()
+    let photoCenterY = (bounds.top + bounds.bottom) / 2
+    translate(photo, '-50%', `${-(photoCenterY - windowMidY) / 2}px`)
   }
-  // console.log(windowHeight)
+  for (let blurb of blurbs) {
+    let bounds = blurb.getBoundingClientRect()
+    let blurbCenterY = (bounds.top + bounds.bottom) / 2
+    translate(blurb, '0px', `${-(blurbCenterY - windowMidY) / 4}px`)
+  }
   hasRequestedFrame = false
 }
 
-function translateY(elm, value) {
-  var translate = `translate3d(0px, ${value}px, 0px)`
+function translate(elm, x, y) {
+  var translate = `translate3d(${x}, ${y}, 0px)`
   elm.style['-webkit-transform'] = translate
   elm.style['-moz-transform'] = translate
   elm.style['-ms-transform'] = translate
@@ -35,12 +42,13 @@ function requestFrame() {
 }
 
 function handleScroll() {
-  lastScrollY = window.pageYOffset
+  scrollY = window.pageYOffset
   requestFrame()
 }
 
 function handleResize() {
   windowHeight = window.innerHeight
+  windowMidY = windowHeight / 2
 }
 
 let initialized = false
@@ -51,6 +59,7 @@ function init() {
   initialized = true
   window.addEventListener('scroll', handleScroll, false)
   window.addEventListener('resize', handleResize, false)
+  updatePositions()
 }
 
 if (document.readyState === 'complete') {
