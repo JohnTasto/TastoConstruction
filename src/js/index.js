7c94d45aefccd2f0e5f9af10cfd5802ebbd523db
John Tasto <john@tasto.net>
2016-06-25 17:52:32 -0700

    Fix window resize bug

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,17 @@ import 'babel-polyfill'
 import AnimationFrame from 'animation-frame'
 const animationFrame = new AnimationFrame()
 
+
 let windowHeight, windowHalfHeight
+let hasRequestedFrame = false
 const photos = document.querySelectorAll('.window-contents img, .garage-contents img')
 const blurbs = document.querySelectorAll('.window-contents p, .garage-contents p')
+window.addEventListener('scroll', handleScroll, false)
+window.addEventListener('resize', handleResize, false)
+
+
 handleResize()
-let hasRequestedFrame = false
+
 
 function updatePositions() {
   let sY = scrollY()
@@ -20,6 +26,15 @@ function updatePositions() {
     translate(blurb, '0px', `${-((blurb._centerY - sY) - windowHalfHeight) / 4}px`)
   }
   hasRequestedFrame = false
+}
+
+function resetPositions() {
+  for (let photo of photos) {
+    translate(photo, '-50%', '0px')
+  }
+  for (let blurb of blurbs) {
+    translate(blurb, '0px', '0px')
+  }
 }
 
 function translate(elm, x, y) {
@@ -45,6 +60,7 @@ function handleScroll() {
 function handleResize() {
   windowHeight = window.innerHeight
   windowHalfHeight = windowHeight / 2
+  resetPositions()
   let sY = scrollY()
   for (let photo of photos) {
     let bounds = photo.getBoundingClientRect()
@@ -55,22 +71,4 @@ function handleResize() {
     blurb._centerY = sY + ((bounds.top + bounds.bottom) / 2)
   }
   handleScroll()
-}
-
-let initialized = false
-function init() {
-  if (initialized) return
-  document.removeEventListener('DOMContentLoaded', init, false)
-  window.removeEventListener('load', init, false)
-  initialized = true
-  window.addEventListener('scroll', handleScroll, false)
-  window.addEventListener('resize', handleResize, false)
-  updatePositions()
-}
-
-if (document.readyState === 'complete') {
-  setTimeout(init)
-} else {
-  document.addEventListener('DOMContentLoaded', init, false)
-  window.addEventListener('load', init, false)
 }
